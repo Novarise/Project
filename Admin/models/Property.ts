e0b9ce26b96a5e9512abd4a4358b9240ea787206
achaab <achaab@pdx.edu>
2024-06-15 00:02:59 -0700

    Ensured Required Fields: Added required: true to label, name, and value fields to ensure they are always provided, enhancing data integrity.
    Comments: Retained comments to indicate potential future references (store).

--- Admin/models/Property.ts
+++ Admin/models/Property.ts
@@ -1,24 +1,43 @@
 import mongoose, { model, Schema, models } from "mongoose";
 
-
+// Define the Property schema with necessary fields and references
 const PropertySchema = new Schema({
-  userId: { type: String, required: true },
-  storeId: { type: String, required: true },
-  //   store: {
-  //     type: mongoose.Schema.Types.ObjectId,
-  //     ref: 'Store', 
-  //   },
+  userId: {
+    type: String,
+    required: true,
+  },
+  storeId: {
+    type: String,
+    required: true,
+  },
+  // If the store reference is needed in the future, uncomment the following:
+  // store: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'Store',
+  // },
   properties: [
     {
-      label: { type: String },
-      values: [{
-        name: { type: String },
-        value: { type: String },
-        }]
+      label: {
+        type: String,
+        required: true,
+      },
+      values: [
+        {
+          name: {
+            type: String,
+            required: true,
+          },
+          value: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
     },
-  ]
+  ],
 }, {
   timestamps: true,
 });
 
+// Export the Property model, creating it if it doesn't already exist
 export const Property = models.Property || model('Property', PropertySchema);
