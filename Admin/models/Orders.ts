75d487cf8b221f52d1f7d7908b2413c49218c1e8
achaab <achaab@pdx.edu>
2024-06-14 23:47:45 -0700

    Structured imports and simplified _id generation. Added comments to enhance code documentation and readability.

--- Admin/models/Orders.ts
+++ Admin/models/Orders.ts
@@ -1,55 +1,59 @@
-import mongoose, { Schema, model, models } from "mongoose";
+import mongoose from "mongoose";
+const { Schema, model, models, Types } = mongoose;
 
+// Define OrderItemSchema
 const OrderItemSchema = new Schema({
-    _id: {
-      type: String,
-      default: () => new mongoose.Types.ObjectId().toString(),
-    },
-    orderId: {
-      type: String,
-      required: true,
-    },
-    productId: {
-      type: String,
-      required: true,
-    },
-    detailId: {
-      type: String,
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
-  }, {
-    timestamps: true
-  });
+  _id: {
+    type: String,
+    default: () => new Types.ObjectId().toString(),
+  },
+  orderId: {
+    type: String,
+    required: true,
+  },
+  productId: {
+    type: String,
+    required: true,
+  },
+  detailId: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+}, {
+  timestamps: true,
+});
 
+// Define OrderSchema
 const OrderSchema = new Schema({
-    _id: {
-      type: String,
-      default: () => new mongoose.Types.ObjectId().toString(),
-    },
-    storeId: {
-      type: String,
-      required: true
-    },
-    orderItems: [OrderItemSchema],
-    isPaid: {
-      type: Boolean,
-      default: false
-    },
-    phone: {
-      type: String,
-      default: ""
-    },
-    address: {
-      type: String,
-      default: ""
-    },
-  }, {
-    timestamps: true
-  });
+  _id: {
+    type: String,
+    default: () => new Types.ObjectId().toString(),
+  },
+  storeId: {
+    type: String,
+    required: true,
+  },
+  orderItems: [OrderItemSchema],
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
+  phone: {
+    type: String,
+    default: "",
+  },
+  address: {
+    type: String,
+    default: "",
+  },
+}, {
+  timestamps: true,
+});
 
+// Export models
 export const OrderItem = models.OrderItem || model('OrderItem', OrderItemSchema);
 export const Order = models.Order || model('Order', OrderSchema);
